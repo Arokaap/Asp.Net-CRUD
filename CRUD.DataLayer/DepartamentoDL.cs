e8143913bcf07a167dcf23730af9f926a671558c
Aaron Aragon <aaron.david.nagore@gmail.com>
2023-09-15 20:54:13 +0200

    feat(DL/EL): Entity layer and data layer added.

---
+++ CRUD.DataLayer/DepartamentoDL.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CRUD.EntityLayer;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CRUD.DataLayer
+{
+    public class DepartamentoDL
+    {
+        public List<Departamento> Lista()
+        {
+            List<Departamento> lista = new List<Departamento>();
+
+            using (SqlConnection oConexion = new SqlConnection(Conexion.cadena))
+            {
+                SqlCommand cmd = new SqlCommand("SELECT * FROM fn_departamentos()", oConexion);
+                cmd.CommandType = CommandType.Text;
+                try
+                {
+                    oConexion.Open();
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            lista.Add(
+                                new Departamento
+                                {
+                                    IdDepartamento = Convert.ToInt32(
+                                        dr["IdDepartamento"].ToString()
+                                    ),
+                                    Nombre = dr["Nombre"].ToString()
+                                }
+                            );
+                        }
+                    }
+
+                    return lista;
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                finally
+                {
+                    oConexion.Close();
+                }
+            }
+        }
+    }
+}
---
+++ CRUD.DataLayer/EmpleadoDL.cs
@@ -0,0 +1,213 @@
+﻿using CRUD.EntityLayer;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD.DataLayer
+{
+    public class EmpleadoDL
+    {
+        public List<Empleado> Lista()
+        {
+            List<Empleado> lista = new List<Empleado>();
+
+            using (SqlConnection oConexion = new SqlConnection(Conexion.cadena))
+            {
+                SqlCommand cmd = new SqlCommand("SELECT * FROM fn_empleados()", oConexion);
+                cmd.CommandType = CommandType.Text;
+
+                try
+                {
+                    oConexion.Open();
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            lista.Add(
+                                new Empleado
+                                {
+                                    IdEmpleado = Convert.ToInt32(dr["IdEmpleado"].ToString()),
+                                    NombreCompleto = dr["NombreCompleto"].ToString(),
+                                    Departamento = new Departamento
+                                    {
+                                        IdDepartamento = Convert.ToInt32(
+                                            dr["IdDepartamento"].ToString()
+                                        ),
+                                        Nombre = dr["Nombre"].ToString()
+                                    },
+                                    Sueldo = (decimal)dr["Sueldo"],
+                                    FechaContrato = dr["FechaContrato"].ToString()
+                                }
+                            );
+                        }
+                    }
+
+                    return lista;
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                finally
+                {
+                    oConexion.Close();
+                }
+            }
+        }
+
+        public Empleado Obtener(int IdEmpleado)
+        {
+            Empleado entidad = new Empleado();
+
+            using (SqlConnection oConexion = new SqlConnection(Conexion.cadena))
+            {
+                SqlCommand cmd = new SqlCommand(
+                    "SELECT * FROM fn_empleado(@idEmpleado)",
+                    oConexion
+                );
+                cmd.Parameters.AddWithValue("@idEmpleado", IdEmpleado);
+                cmd.CommandType = CommandType.Text;
+
+                try
+                {
+                    oConexion.Open();
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        if (dr.Read())
+                        {
+                            entidad.IdEmpleado = Convert.ToInt32(dr["IdEmpleado"].ToString());
+                            entidad.NombreCompleto = dr["NombreCompleto"].ToString();
+                            entidad.Departamento = new Departamento
+                            {
+                                IdDepartamento = Convert.ToInt32(dr["IdDepartamento"].ToString()),
+                                Nombre = dr["Nombre"].ToString()
+                            };
+                            entidad.Sueldo = (decimal)dr["Sueldo"];
+                            entidad.FechaContrato = dr["FechaContrato"].ToString();
+                        }
+                    }
+
+                    return entidad;
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                finally
+                {
+                    oConexion.Close();
+                }
+            }
+        }
+
+        public bool Crear(Empleado entidad)
+        {
+            bool respuesta = false;
+
+            using (SqlConnection oConexion = new SqlConnection(Conexion.cadena))
+            {
+                SqlCommand cmd = new SqlCommand("sp_CrearEmpleado", oConexion);
+                cmd.Parameters.AddWithValue("@NombreCompleto", entidad.NombreCompleto);
+                cmd.Parameters.AddWithValue("@IdDepartamento", entidad.Departamento);
+                cmd.Parameters.AddWithValue("@Sueldo", entidad.Sueldo);
+                cmd.Parameters.AddWithValue("@FechaContrato", entidad.FechaContrato);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                try
+                {
+                    oConexion.Open();
+                    int filasAfectadas = cmd.ExecuteNonQuery();
+
+                    if (filasAfectadas > 0)
+                    {
+                        respuesta = true;
+                    }
+
+                    return respuesta;
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                finally
+                {
+                    oConexion.Close();
+                }
+            }
+        }
+
+        public bool Editar(Empleado entidad)
+        {
+            bool respuesta = false;
+
+            using (SqlConnection oConexion = new SqlConnection(Conexion.cadena))
+            {
+                SqlCommand cmd = new SqlCommand("sp_EditarEmpleado", oConexion);
+                cmd.Parameters.AddWithValue("@IdEmpleado", entidad.IdEmpleado);
+                cmd.Parameters.AddWithValue("@NombreCompleto", entidad.NombreCompleto);
+                cmd.Parameters.AddWithValue("@IdDepartamento", entidad.Departamento);
+                cmd.Parameters.AddWithValue("@Sueldo", entidad.Sueldo);
+                cmd.Parameters.AddWithValue("@FechaContrato", entidad.FechaContrato);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                try
+                {
+                    oConexion.Open();
+                    int filasAfectadas = cmd.ExecuteNonQuery();
+
+                    if (filasAfectadas > 0)
+                    {
+                        respuesta = true;
+                    }
+
+                    return respuesta;
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                finally
+                {
+                    oConexion.Close();
+                }
+            }
+        }
+
+        public bool Eliminar(int IdEmpleado)
+        {
+            bool respuesta = false;
+
+            using (SqlConnection oConexion = new SqlConnection(Conexion.cadena))
+            {
+                SqlCommand cmd = new SqlCommand("sp_EliminarEmpleado", oConexion);
+                cmd.Parameters.AddWithValue("@IdEmpleado", IdEmpleado);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                try
+                {
+                    oConexion.Open();
+                    int filasAfectadas = cmd.ExecuteNonQuery();
+
+                    if (filasAfectadas > 0)
+                    {
+                        respuesta = true;
+                    }
+
+                    return respuesta;
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                finally
+                {
+                    oConexion.Close();
+                }
+            }
+        }
+    }
+}
